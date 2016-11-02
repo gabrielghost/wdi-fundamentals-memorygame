4bf639f00cec63e7608bb3a81473752d5950e556
CharlieGavins <charlesgavins@gmail.com>
2016-11-02 16:17:03 +0000

    Added broken alerts

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,5 +1,10 @@
-console.log("JS file is connected to HTML! Woo!")
-var cardOne = "Queen"
-var cardTwo = "Queen"
-var cardThree = "King"
-var cardFour = "King"
+console.log("JS file is connected to HTML! Woo!");
+var cardOne = "Queen";
+var cardTwo = "Queen";
+var cardThree = "King";
+var cardFour = "King";
+if (cardTwo === cardFour){
+alert("You found a match!")
+}else if (cardTwo !== cardFour){
+	alert("Sorry, try again.")
+}
